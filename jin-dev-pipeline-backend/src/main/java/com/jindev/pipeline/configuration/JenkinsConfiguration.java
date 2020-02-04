e12a09c12aa25ed1d1918b4f7a1917716c880c88
MINJI KIM <rlaalswl627@gmail.com>
2020-02-04 23:04:31 +0900

    Added Kotlin library, Jenkins Configuration

---
+++ jin-dev-pipeline-backend/src/main/java/com/jindev/pipeline/configuration/JenkinsConfiguration.java
@@ -0,0 +1,26 @@
+package com.jindev.pipeline.configuration;
+
+import com.offbytwo.jenkins.JenkinsServer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Configuration
+public class JenkinsConfiguration {
+  @Value("jenkins.url")
+  private String jenkinsUrl;
+
+  @Value("jenkins.id")
+  private String jenkinsId;
+
+  @Value("jenkins.password")
+  private String jenkinsPassword;
+
+  @Bean
+  public JenkinsServer jenkins() throws URISyntaxException {
+    return new JenkinsServer(new URI(jenkinsUrl), jenkinsId, jenkinsPassword);
+  }
+}
