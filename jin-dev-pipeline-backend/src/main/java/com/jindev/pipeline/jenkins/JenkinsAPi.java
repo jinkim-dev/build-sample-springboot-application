ea1cf6002503b7020ad26c9f6c3d1b496f3f74d7
MINJI KIM <rlaalswl627@gmail.com>
2020-02-06 23:23:24 +0900

    Added JenkinsApi execute method

--- jin-dev-pipeline-backend/src/main/java/com/jindev/pipeline/jenkins/JenkinsAPi.java
+++ jin-dev-pipeline-backend/src/main/java/com/jindev/pipeline/jenkins/JenkinsAPi.java
@@ -9,13 +9,51 @@ import java.io.IOException;
 @Component
 public class JenkinsAPi {
 
-    private JenkinsServer jenkins;
+  private JenkinsServer jenkins;
 
-    public JenkinsAPi(JenkinsServer jenkins) {
-        this.jenkins = jenkins;
+  public JenkinsAPi(JenkinsServer jenkins) {
+    this.jenkins = jenkins;
+  }
+
+  public JobWithDetails getJob(String jobName) {
+    return executeWithResult(() -> jenkins.getJob(jobName));
+  }
+
+  public void run(String jobName, String jobXml) {
+    execute(() -> jenkins.createJob(jobName, jobXml));
+  }
+
+  public void execute(Executor executor) {
+    executor.exec();
+  }
+
+  public <R> R executeWithResult(ExecutorWithResult executor) {
+    return (R) executor.exec();
+  }
+
+  @FunctionalInterface
+  public interface Executor<E extends IOException> {
+    default void exec() throws RuntimeException {
+      try {
+        run();
+      } catch (Exception ex) {
+        throw new RuntimeException(ex);
+      }
     }
 
-    public JobWithDetails getJo(String jobName) throws IOException {
-        return jenkins.getJob(jobName);
+    void run() throws E;
+  }
+
+  @FunctionalInterface
+  public interface ExecutorWithResult<T, E extends IOException> {
+    default <T> T exec() throws RuntimeException {
+      try {
+        return (T) run();
+      } catch (Exception ex) {
+        throw new RuntimeException(ex);
+      }
     }
+
+    T run() throws E;
+  }
 }
