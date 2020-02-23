1df571e3f14a876a814969e50fda1251c6ce1444
MINJI KIM <rlaalswl627@gmail.com>
2020-02-23 23:10:13 +0900

    Fixed render jenkins job template

--- jin-dev-pipeline-backend/src/main/java/com/jindev/pipeline/api/build/BuildServiceImpl.java
+++ jin-dev-pipeline-backend/src/main/java/com/jindev/pipeline/api/build/BuildServiceImpl.java
@@ -1,5 +1,7 @@
 package com.jindev.pipeline.api.build;
 
+import com.google.common.collect.Maps;
+import com.hubspot.jinjava.Jinjava;
 import com.jindev.pipeline.jenkins.JenkinsAPi;
 import com.offbytwo.jenkins.model.JobWithDetails;
 import com.offbytwo.jenkins.model.QueueReference;
@@ -12,6 +14,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -19,12 +22,13 @@ import java.util.Optional;
 public class BuildServiceImpl implements BuildService {
 
   private JenkinsAPi jenkinsAPi;
-
   private BuildDao buildDao;
+  private Jinjava jinjava;
 
-  public BuildServiceImpl(JenkinsAPi jenkinsAPi, BuildDao buildDao) {
+  public BuildServiceImpl(JenkinsAPi jenkinsAPi, BuildDao buildDao, Jinjava jinjava) {
     this.jenkinsAPi = jenkinsAPi;
     this.buildDao = buildDao;
+    this.jinjava = jinjava;
   }
 
   @Override
@@ -44,10 +48,17 @@ public class BuildServiceImpl implements BuildService {
 
   @Override
   public void createJob(String jobName) {
-    ClassPathResource resource = new ClassPathResource("/templates/jenkins/config.xml");
+    ClassPathResource configResource = new ClassPathResource("/templates/jenkins/config.xml");
+    ClassPathResource pipelineResource = new ClassPathResource("/templates/jenkins/jenkinsfile");
     try {
-      Path path = Paths.get(resource.getURI());
-      String jobXml = new String(Files.readAllBytes(path));
+      Path pipelinePath = Paths.get(pipelineResource.getURI());
+      String pipeline = new String(Files.readAllBytes(pipelinePath));
+      // TODO: Render pipeline
+      Path configPath = Paths.get(configResource.getURI());
+      String jobTemplateXml = new String(Files.readAllBytes(configPath));
+      Map<String, Object> context = Maps.newHashMap();
+      context.put("script", pipeline);
+      String jobXml = jinjava.render(jobTemplateXml, context);
       jenkinsAPi.createJob(jobName, jobXml);
     } catch (IOException e) {
       e.printStackTrace();
--- jin-dev-pipeline-backend/src/main/java/com/jindev/pipeline/configuration/WebConfiguration.java
+++ jin-dev-pipeline-backend/src/main/java/com/jindev/pipeline/configuration/WebConfiguration.java
@@ -1,5 +1,6 @@
 package com.jindev.pipeline.configuration;
 
+import com.hubspot.jinjava.Jinjava;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,8 +8,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class WebConfiguration {
 
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
+  @Bean
+  public ModelMapper modelMapper() {
+    return new ModelMapper();
+  }
+
+  @Bean
+  public Jinjava jinjava() {
+    return new Jinjava();
+  }
 }
